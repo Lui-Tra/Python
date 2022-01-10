1072fd827e8a054efee10b010c1565a8e15f2fc3
FlorianNAdam <nalu.adam@yahoo.com>
2022-01-10 20:56:58 +0100

    add multichar-token recognition to parser

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from syntaxtree import *
 import re
+
+operators = {
+    "not": "¬",
+    "and": "∧",
+    "or": "∨",
+    "xor": "⊕",
+    "implication": "→",
+    "bi-conditional": "↔",
+    "ite": "ITE"
+}
+
+
+class Operator:
+    def __init__(self, children):
+        pass
+
+
+class Variable:
+    def __init__(self, name):
+        pass
 
 
 def remove_parenthesis(token):
@@ -23,6 +42,18 @@ def remove_parenthesis(token):
         return token
 
 
+def find_operator(formel, index):
+    char = formel[index]
+    if char in operators.values():
+        return char
+    else:
+        for operator in operators.values():
+            if index + (len(operator) - 1) <= len(formel):
+                if operator.startswith(char):
+                    if formel[index:index + len(operator)] == operator:
+                        return operator
+        return None
+
 def parse(formel):
     formel = re.sub(r"\s", "", formel)
     formel = remove_parenthesis(formel)
@@ -35,13 +66,16 @@ def parse(formel):
             indent_level += 1
         elif char == ")":
             indent_level -= 1
-        elif indent_level == 0 and char in operators.values():
-            binding_priority = list(operators.values()).index(char)
-            if binding_priority > min_binding_priority:
-                min_binding_priority = binding_priority
+        elif indent_level == 0:
+            operator = find_operator(formel, index)
+            if operator is not None:
+                binding_priority = list(operators.values()).index(operator)
+                if binding_priority > min_binding_priority:
+                    min_binding_priority = binding_priority
         index += 1
     if indent_level != 0:
         raise ValueError("invalid")
+
     index = 0
     indent_level = 0
     current_token = ""
@@ -55,32 +89,33 @@ def parse(formel):
         elif char == ")":
             current_token += char
             indent_level -= 1
-        elif indent_level == 0 and char == seperator:
+        elif indent_level == 0 and find_operator(formel, index) == seperator:
             tokens.append(remove_parenthesis(current_token))
             current_token = ""
+            index += len(seperator)-1
         else:
             current_token += char
         index += 1
     tokens.append(remove_parenthesis(current_token))
+
+    return
+
     if len(tokens) == 1:
         return Variable(tokens[0])
     else:
         if seperator == "¬":
-            op = Operator([parse(tokens[-1])], "¬")
+            op = Operator([parse(tokens[-1])])
             print(len(tokens))
             for i in range(len(tokens) - 2):
-                op = Operator([op], "¬")
+                op = Operator([op])
             return op
         else:
-            return Operator([parse(token) for token in tokens], seperator)
+            return Operator([parse(token) for token in tokens])
 
 
 if __name__ == "__main__":
     print("test")
-    formel = "¬a ∧ b ∨ c ∧ b"
+    formel = "¬a ∧ b ITE c ∧ b"
 
     root = parse(formel)
-    root.traverse()
-    root = root.simplify().simplify()
-    print()
-    root.traverse()
+
