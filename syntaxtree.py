78a8635f70cd14f44fc72cf6b2ae578ef8157585
Kilian Northoff <ge95bit@mytum.de>
2022-01-11 21:21:53 +0100

    kv diagramm

--- syntaxtree.py
+++ syntaxtree.py
@@ -1,8 +1,12 @@
+from kv import show_kv_diagramm
 from parser import Parser
 from termcolor import colored
 
-
 if __name__ == "__main__":
-    f = Parser().parse("not A")
-
-    f.print_truth_table()
+    show_kv_diagramm(
+        [[0, 1, 0, 1],
+         [1, 1, 0, 0],
+         [1, 1, 0, 0],
+         [1, 1, 0, 0]],
+        scale=2
+    )
