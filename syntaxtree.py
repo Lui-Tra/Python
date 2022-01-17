2dbf54fc34f96de1179609c265453eab1eea57cd
Kilian Northoff <ge95bit@mytum.de>
2022-01-17 22:15:24 +0100

    simplify

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,6 +2,6 @@ import parser
 
 
 if __name__ == "__main__":
-    formula = parser.parse("((q ∨ t) → (p ∨ t)) ∨ (q ∧ (t ↔ p))")
+    formula = parser.parse("(((¬q ∧ ¬t) ∨ (p ∨ t)) ∨ (q ∧ ((¬t ∨ p) ∧ (¬p ∨ t)))) ∨ (((q ∨ t) ∧ (¬p ∧ ¬t)) ∧ (¬q ∨ ((t ∧ ¬p) ∨ (p ∧ ¬t))))")
     formula.simplify().nnf()
     print(formula)
