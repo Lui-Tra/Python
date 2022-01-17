d874c054697f996069aef34aa10230162f9e03ee
Kilian Northoff <ge95bit@mytum.de>
2022-01-17 21:43:19 +0100

    simplify

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,7 +2,7 @@ import parser
 
 
 if __name__ == "__main__":
-    formula = parser.parse("(A or F or Q) and (B or C or D)")
+    formula = parser.parse("(A or F or Q) and (B or C or D) and (G or V or X)")
     print(formula)
     formula.simplify()
     print(formula)
