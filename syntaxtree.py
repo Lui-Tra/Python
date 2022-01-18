62191bfd0a5db654224b1ee693d060d590aabf16
Kilian Northoff <ge95bit@mytum.de>
2022-01-18 21:32:56 +0100

    nand und nor

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,7 +2,11 @@ import parser
 
 
 if __name__ == "__main__":
-    formula = parser.parse("c or d and f")
+    formula = parser.parse("a nand b")
+    print(formula)
+    formula.to_nand()
+    print(formula)
+    formula.to_nand()
     print(formula)
     formula.to_nand()
     print(formula)
