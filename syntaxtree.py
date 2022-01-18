94e658d17aed8dc12466c00b3bdee3df2fa9d0a4
Kilian Northoff <ge95bit@mytum.de>
2022-01-18 20:59:42 +0100

    simplify

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,8 +2,8 @@ import parser
 
 
 if __name__ == "__main__":
-    # formula = parser.parse("(((¬q ∧ ¬t) ∨ (p ∨ t)) ∨ (q ∧ ((¬t ∨ p) ∧ (¬p ∨ t)))) ∨ (((q ∨ t) ∧ (¬p ∧ ¬t)) ∧ (¬q ∨ ((t ∧ ¬p) ∨ (p ∧ ¬t))))")
-    formula = parser.parse("(a and b) or not a")
+    formula = parser.parse("(((¬q ∧ ¬t) ∨ (p ∨ t)) ∨ (q ∧ ((¬t ∨ p) ∧ (¬p ∨ t)))) ∨ (((q ∨ t) ∧ (¬p ∧ ¬t)) ∧ (¬q ∨ ((t ∧ ¬p) ∨ (p ∧ ¬t))))")
+    # formula = parser.parse("(a and b) or not a")
     print(formula)
-    formula.root = formula.root.smart_expand()
+    formula.simplify()
     print(formula)
