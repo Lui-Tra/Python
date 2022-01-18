77785b5e3746094dd0f56942b437606629183203
Kilian Northoff <ge95bit@mytum.de>
2022-01-18 22:56:31 +0100

    nand und nor

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,5 +2,10 @@ import parser
 
 
 if __name__ == "__main__":
-    form = parser.parse("(a and b and e) or (b and a and d) or (d and f)").simplify()
+    form = parser.parse("(a and b and e) or (b and a and d) or (d and f)")
+    form.simplify()
+    print(form)
+
+    form = parser.parse("(a and b and e) or (b and a and d) or (a and f)")
+    form.simplify()
     print(form)
