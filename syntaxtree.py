042dc09fbd4a8ce5b3ac83811f8a448ab5e15afa
FlorianNAdam <nalu.adam@yahoo.com>
2022-01-18 23:03:04 +0100

    add indented_print example

--- syntaxtree.py
+++ syntaxtree.py
@@ -1,4 +1,12 @@
 import parser
+from debugging import indented_print
+
+def test2():
+    indented_print("hi")
+
+def test():
+    indented_print("hi")
+    test2()
 
 
 if __name__ == "__main__":
@@ -9,3 +17,6 @@ if __name__ == "__main__":
     form = parser.parse("(a and b and e) or (b and a and d) or (a and f)")
     form.simplify()
     print(form)
+
+    indented_print("test")
+    test()
