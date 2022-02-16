aa32a7c790b6abb958c573f85245bfd056c98d36
kilian <ge95bit@mytum.de>
2022-02-16 11:40:47 +0100

    EEA (zwar keine Logik aber nützlich für Klausur)

--- syntaxtree.py
+++ syntaxtree.py
@@ -2,8 +2,6 @@ from dpll_tree import create_dpll_tree, display_dpll_tree
 import parser
 
 if __name__ == "__main__":
-    form = parser.parse("{{p, ¬w}, {p, y}, {¬p, ¬r, ¬w, y}, {r}, {¬r, w, ¬y}, {w, y}, {¬w, ¬y}}")
+    form = parser.parse(" ((w ↔ (y ↔ z)) ↔ (w ∨ (w ∧ y)))")
 
-    root = create_dpll_tree(form.to_clause_list())
-
-    display_dpll_tree(root, 2)
+    form.print_truth_table()
